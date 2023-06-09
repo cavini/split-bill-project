241ad5c5b5fcf798dfd7a63b5b9d8799e499ce89
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2023-06-09 14:21:06 -0300

    feat: added logic to display friend form based on state

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { ButtonComponentProps } from './@types';
 
-const Button = ({ text }: ButtonComponentProps) => {
-  return <button className="button">{text}</button>;
+const Button = ({ text, onClick }: ButtonComponentProps) => {
+  return (
+    <button onClick={onClick} className="button">
+      {text}
+    </button>
+  );
 };
 
 export default Button;
